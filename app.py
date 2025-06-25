c5538313dec60a5384778bd85f1989dbd605fe95
giaNguyen214 <156935874+giaNguyen214@users.noreply.github.com>
2025-06-25 21:17:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'message': 'Test successfully'}), 200
+    
 @app.route('/read_excel', methods=['POST'])
 def read_excel():
     if 'file' not in request.files:
@@ -24,4 +28,6 @@ def read_excel():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
