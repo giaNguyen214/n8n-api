e91dec271eed978d4cd275c8a30792fef6bc94a2
giaNguyen214 <156935874+giaNguyen214@users.noreply.github.com>
2025-06-30 21:54:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def check_pdf_type():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
